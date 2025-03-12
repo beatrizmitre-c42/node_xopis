aed128ceca528cb193a0051926c927020cc3d956
beatrizmitre-c42 <beatriz.mitre@codeminer42.com>
2025-03-12 17:18:01 -0300

    improved types file

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,10 +1,20 @@
+export type OrderWithItemsType = {
+    id?: number;
+    total_paid: number;
+    total_discount: number;
+    status: string;
+    customer_id: number;
+    total_tax?: 0,
+    total_shipping?: 0,
+    items: OrderItemType[];
+    created_at?: Date;
+    updated_at?: Date;
+}
+
 export type OrderItemType = {
     product_id: number;
     quantity: number;
     discount: number;
-};
+}
 
-export type OrderType = {
-    customer_id: number;
-    items: OrderItemType[];
-};
+export type CreateOrderBodyType = Pick<OrderWithItemsType, 'customer_id' | 'items'>
