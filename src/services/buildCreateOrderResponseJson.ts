7c8b9d2121102516196ae98eb47b9ba539b780fe
beatrizmitre-c42 <beatriz.mitre@codeminer42.com>
2025-03-13 17:58:19 -0300

    fixed buildCreateOrderResponseJson as requested on task 1 review

--- src/services/buildCreateOrderResponseJson.ts
+++ src/services/buildCreateOrderResponseJson.ts
@@ -1,23 +1,10 @@
 import Order from "src/models/Order";
 
-export default (order: Order) => {
-    if (order.items) {
-        return {
-            id: order.id,
-            customer_id: order.customer_id,
-            total_paid: order.total_paid,
-            total_discount: order.total_discount,
-            status: order.status,
-            items: order.items.map((item) => { return { product_id: item.product_id, quantity: item.quantity, discount: item.discount } })
-        }
-    }
-    else {
-        return {
-            id: order.id,
-            customer_id: order.customer_id,
-            total_paid: order.total_paid,
-            total_discount: order.total_discount,
-            status: order.status
-        }
-    }
-}
+export default (order: Order) => ({
+    id: order.id,
+    customer_id: order.customer_id,
+    total_paid: order.total_paid,
+    total_discount: order.total_discount,
+    status: order.status,
+    ...(order.items && { items: order.items.map(({ product_id, quantity, discount }) => ({ product_id, quantity, discount })) })
+});
